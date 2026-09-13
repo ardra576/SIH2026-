6cac55ebc5f6c7d27a98a7e685b4c76c54f3a209
ardra576 <ardra6572@gmail.com>
2026-09-13 13:16:14 +0530

    final project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-﻿import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
         secure: false,
       }
